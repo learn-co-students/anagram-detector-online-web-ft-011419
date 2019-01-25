0dfbec42a004c325d3acfe6c3a3a7437c2e6bc57
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-25 21:54:17 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word_arr
+  def initialize(word)
+    @word_arr = word.split
+  end
+  def match(arr)
+    arr.collect do |word|
+      word_arr = @word_arr
+      test_word_arr = word.split
+      test_word_arr.each do |char|
+        if word_arr.include?(char)
+          word_arr - char
+          test_word_arr - char
+        end
+      end
+      word_arr.empty? && test_word_arr.empty?
+    end
+  end
+end
