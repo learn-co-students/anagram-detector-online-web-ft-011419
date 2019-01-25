076e8c5e18e5daf26c45e93d24d27f9e677ed697
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-25 23:29:50 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,20 +1,19 @@
 # Your code goes here!
+require 'pry'
+
 class Anagram
   attr_accessor :word_arr
   def initialize(word)
-    @word_arr = word.split
+    @word_arr = word.split('')
   end
-  def match(arr)
-    arr.collect do |word|
-      word_arr = @word_arr
-      test_word_arr = word.split
-      test_word_arr.each do |char|
-        if word_arr.include?(char)
-          word_arr - char
-          test_word_arr - char
-        end
+  def match(test_array)
+    matches = []
+    test_array.each do |test_word|
+      test_word_array = test_word.split('')
+      if test_word_array.sort == @word_arr.sort
+        matches << test_word
       end
-      word_arr.empty? && test_word_arr.empty?
     end
+    matches
   end
 end
