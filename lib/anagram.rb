c87f1539e4a460884535182e31bc984b48fd07ab
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-25 23:30:39 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,13 +7,9 @@ class Anagram
     @word_arr = word.split('')
   end
   def match(test_array)
-    matches = []
-    test_array.each do |test_word|
+    test_array.collect do |test_word|
       test_word_array = test_word.split('')
-      if test_word_array.sort == @word_arr.sort
-        matches << test_word
-      end
+      test_word_array.sort == @word_arr.sort
     end
-    matches
   end
 end
